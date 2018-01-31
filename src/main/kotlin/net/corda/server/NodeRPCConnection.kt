69bd2c292007e97f7068469a29aa129f07df9626
joeldudleyr3 <joel.dudley@r3.com>
2018-01-31 11:35:37 +0000

    Address comments about PostConstruct and PreDestroy.

--- src/main/kotlin/net/corda/server/NodeRPCConnection.kt
+++ src/main/kotlin/net/corda/server/NodeRPCConnection.kt
@@ -1,10 +1,13 @@
 package net.corda.server
 
 import net.corda.client.rpc.CordaRPCClient
+import net.corda.client.rpc.CordaRPCConnection
 import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.utilities.NetworkHostAndPort
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+import javax.annotation.PreDestroy
 
 private const val CORDA_USER_NAME = "config.rpc.username"
 private const val CORDA_USER_PASSWORD = "config.rpc.password"
@@ -24,17 +27,26 @@ private const val CORDA_RPC_PORT = "config.rpc.port"
  */
 @Component
 open class NodeRPCConnection(
-        @Value("\${$CORDA_NODE_HOST}") host: String,
-        @Value("\${$CORDA_USER_NAME}") username: String,
-        @Value("\${$CORDA_USER_PASSWORD}") password: String,
-        @Value("\${$CORDA_RPC_PORT}") rpcPort: Int) {
+        @Value("\${$CORDA_NODE_HOST}") private val host: String,
+        @Value("\${$CORDA_USER_NAME}") private val username: String,
+        @Value("\${$CORDA_USER_PASSWORD}") private val password: String,
+        @Value("\${$CORDA_RPC_PORT}") private val rpcPort: Int): AutoCloseable {
 
-    val proxy: CordaRPCOps
+    lateinit var rpcConnection: CordaRPCConnection
+        private set
+    lateinit var proxy: CordaRPCOps
+        private set
 
-    init {
-        val rpcAddress = NetworkHostAndPort(host, rpcPort)
-        val rpcClient = CordaRPCClient(rpcAddress)
-        val rpcConnection = rpcClient.start(username, password)
-        proxy = rpcConnection.proxy
+    @PostConstruct
+    fun initialiseNodeRPCConnection() {
+            val rpcAddress = NetworkHostAndPort(host, rpcPort)
+            val rpcClient = CordaRPCClient(rpcAddress)
+            val rpcConnection = rpcClient.start(username, password)
+            proxy = rpcConnection.proxy
+    }
+
+    @PreDestroy
+    override fun close() {
+        rpcConnection.notifyServerAndClose()
     }
 }
